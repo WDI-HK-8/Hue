89a9269582812f31cd18dbe4470782adeb71ec9d
jonycfu <jon.yc.fu@gmail.com>
2015-08-05 23:24:34 +0800

    feat(route_colors): Add front-end of colors.html, incl. colorpicker, color list, and definitions

--- public/application3.js
+++ public/application3.js
@@ -1,4 +1,8 @@
 $(function(){
   var query_str = window.location.href.slice(window.location.href.indexOf('project') + 8);
   $('.navbar-brand h1').text(query_str);
+  $('#myValue').change(function(event) {
+    $('#colorpicker_form').css('background-color', '#' + $(this).val());
+  });
+
 });
